42bc492f2618e9b5c450905710e2cc08ee472ac7
Mythili Velivelli <mythili123@gmail.com>
2025-06-16 02:33:43 +0530

    Updated

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: Number(process.env.PG_PORT),
+    DATABASE_URL: process.env.PG_DATABASE_URL,
+  // host: process.env.PG_HOST,
+  // database: process.env.PG_DATABASE,
+  // password: process.env.PG_PASSWORD,
+  // port: Number(process.env.PG_PORT),
 });
 
 
