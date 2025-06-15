3acca6630b1604b5bdb315acaf70a75f8dd79b10
Mythili Velivelli <mythili123@gmail.com>
2025-06-16 01:19:38 +0530

    Updated

--- index.js
+++ index.js
@@ -8,12 +8,20 @@ const port = process.env.PORT || 3000;
 import dotenv from "dotenv";
 dotenv.config();
 
+// const pool = new Pool({
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: Number(process.env.PGPORT),
+// });
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: Number(process.env.PGPORT),
+  user: "blogcraft_db_ksdf_user",
+  host: "dpg-d17gri95pdvs738ch2og-a.oregon-postgres.render.com",
+  database: "blogcraft_db_ksdf",
+  password: "rbfIej9oKuLG6cAFyCa6uZlGbaESW4i4",
+  port: 5432,
+  ssl: { rejectUnauthorized: false }
 });
 
 // Middleware
